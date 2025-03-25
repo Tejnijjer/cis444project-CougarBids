931a207e5fbe60beae6d3bd6b52cdd35f75f62f8
chase <chase@rhusoft.com>
2025-03-25 15:26:32 -0700

    changed listings to be dynamically created so database integration is easy

--- listings/script.js
+++ listings/script.js
@@ -1,3 +1,32 @@
 
+window.onload = function(){
+    //add database functionality here
+    for(let i=0; i<1000; i++){
+        var listings = document.getElementById('listings');
+        var itemBox = document.createElement('div');
+        itemBox.className = 'item_box';
 
+        var itemImage = document.createElement('div');
+        itemImage.className = 'item-image';
+        itemImage.textContent = 'Item Image';
+
+        var itemDescription = document.createElement('div');
+        itemDescription.className = 'item-description';
+        itemDescription.textContent = 'Item Description';
+
+        var itemPrice = document.createElement('div');
+        itemPrice.className = 'item-price';
+        itemPrice.textContent = 'Price';
+
+        var listingButton = document.createElement('button'+i);
+        listingButton.className = 'listing-button';
+
+        itemBox.appendChild(itemImage);
+        itemBox.appendChild(itemDescription);
+        itemBox.appendChild(itemPrice);
+        itemBox.appendChild(listingButton);
+
+        listings.appendChild(itemBox);
+    }
+}
 
