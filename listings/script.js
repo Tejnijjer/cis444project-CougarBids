c7d8f86dca916f742de6584d2b0f7e8a684df229
crhubottom <chase.rhubottom@gmail.com>
2025-03-25 19:16:00 -0700

    Added create listing page

--- listings/script.js
+++ listings/script.js
@@ -8,11 +8,10 @@ window.onload = function(){
 
         var itemImage = document.createElement('div');
         itemImage.className = 'item-image';
-        itemImage.textContent = 'Item Image';
-
+        itemImage.innerHTML='<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3vrTUU3CKbUDThpm8aZzFXdTmai6PodNfXA&s" alt="Item Image">';
         var itemDescription = document.createElement('div');
         itemDescription.className = 'item-description';
-        itemDescription.textContent = 'Item Description';
+        itemDescription.textContent = 'Placeholder Item '+i;
 
         var itemPrice = document.createElement('div');
         itemPrice.className = 'item-price';
@@ -40,7 +39,18 @@ function goToProfile(){
     alert('Go to profile');
 }
 function addListing(){
-    alert('Add listing');
+    const modal = document.getElementById('createListingModal');
+    modal.style.display = 'flex';
+}
+function closeCreateListing(){
+    const modal = document.getElementById('createListingModal');
+    modal.style.display = 'none';
+}
+window.onclick = function(event) {
+    const modal = document.getElementById('createListingModal');
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
 }
 function goToHome(){
     document.getElementById('listings').scrollTo(0,0)
