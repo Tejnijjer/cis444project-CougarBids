8203b9af8e78e57ff864bf768e5e1a47f2e33559
SleepyKayOS <sleepykayos@gmail.com>
2025-03-26 22:45:57 -0700

    Settings - clickable nav buttons, not functional delete button

---
+++ settings-page/settings.js
@@ -0,0 +1,11 @@
+
+function goToPage(page) {
+    window.location.href = page;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    let btn = document.getElementById("delete-button");
+    btn.addEventListener("click", function () {
+        alert("not functional yet");
+    });
+});
